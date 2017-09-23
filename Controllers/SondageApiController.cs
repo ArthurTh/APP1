9d0f3f110e144d990237839acc27d5bf1681280c
Arthur Thomassine <thoa2602@usherbrooke.ca>
2017-09-23 15:47:12 -0400

    Initial commit

---
+++ Controllers/SondageApiController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SondageApi.Model;
+
+namespace SondageApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class SondagesController : Controller
+    {
+        private readonly SondageContext _context;
+        public SondagesController(SondageContext context)
+        {
+            _context = context;
+
+            if (_context.SimpleSondageDAOs.Count() == 0)
+            {
+                _context.SimpleSondageDAOs.Add(new SimpleSondageDAO());
+                _context.SaveChanges();
+            }
+        }
+
+        // GET api/sondages
+        [HttpGet]
+        public IActionResult<string> Get()
+        {
+            return new string[] { "Quel sondage choisissez vous ?", "1", "2" };
+        }
+
+        [HttpGet("{id}", Name = "GetSondage")]
+        public IActionResult GetSondageById(int id)
+        {
+            var dbSondage = _context.SimpleSondageDAOs.FirstOrDefault(t => t.Id == 1);
+            if (dbSondage == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(dbSondage);
+        }
+
+       
+
+        // GET api/sondages/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/sondages
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/sondages/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/sondages/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Database.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SondageApi.Model;
+
+namespace SondageApi.Model
+{
+    public class SondageContext : DbContext
+    {
+        public SondageContext(DbContextOptions<SondageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SimpleSondageDAO> SimpleSondageDAOs { get; set; }
+
+    }
+}
