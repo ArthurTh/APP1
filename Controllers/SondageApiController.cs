bc06da82349d0e761993945a72273692b7327b4f
Arthur Thomassine <thoa2602@usherbrooke.ca>
2017-09-25 03:26:40 -0400

    Web API 1.0 + Client 1.0 (sans Token)

--- Controllers/SondageApiController.cs
+++ Controllers/SondageApiController.cs
@@ -5,90 +5,80 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SondageApi.Model;
 using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
 
 namespace SondageApi.Controllers
 {
     [Route("api/[controller]")]
     public class SondagesController : Controller
     {
-        private readonly SondageContext _context;
-        public SondagesController(SondageContext context)
-        {
-            _context = context;
+                                                                                                /*private readonly SondageContext _context;
+                                                                                                public SondagesController(SondageContext context)
+                                                                                                {
+                                                                                                    _context = context;
 
-            if (_context.SimpleSondageDAOs.Count() == 0)
-            {
-                _context.SimpleSondageDAOs.Add(new SimpleSondageDAO());
-                _context.SaveChanges();
-            }
-        }
+                                                                                                    if (_context.SimpleSondageDAOs.Count() == 0)
+                                                                                                    {
+                                                                                                        _context.SimpleSondageDAOs.Add(new SimpleSondageDAO());
+                                                                                                        _context.SaveChanges();
+                                                                                                    }
+                                                                                                }*/
+        public readonly ISondageDAO sondage = new SimpleSondageDAO();
 
         // GET api/sondages
-        
         [HttpGet]
         public IActionResult GetSondageAvailable()
         {
-            String result = null;
-            var dbSondage = _context.SimpleSondageDAOs.FirstOrDefault(t => t.Id == 1);
-            if (dbSondage == null)
-            {
-                return NotFound();
-            }
-            
-            foreach(Poll sondage in dbSondage.GetAvailablePolls())
-            {
-                result = result + "Sondage numéro : " + sondage.Id 
-                    + Environment.NewLine + "Description : " + sondage.Description 
-                    + Environment.NewLine;
-            }
-            return new ObjectResult(result);
+                                                                                                // Génération d'un objet sondage
+                                                                                                //ISondageDAO sondage = new SimpleSondageDAO();
+            // Récupération des descriptions
+            IList<Poll> question = sondage.GetAvailablePolls();
+            // Formatage
+            string description = JsonConvert.SerializeObject(question);
+            // Retour de GET
+            if (!description.Equals(null)) return Ok(description);
+            else return NotFound();
+                                                                                                // Formatage de la réponse
+                                                                                                /*String result = null;
+                                                                                                foreach (Poll sondage in dbSondage.GetAvailablePolls())
+                                                                                                {
+                                                                                                    result = result + "Sondage numéro : " + sondage.Id 
+                                                                                                        + Environment.NewLine + "Description : " + sondage.Description 
+                                                                                                        + Environment.NewLine;
+                                                                                                }
+                                                                                                return new ObjectResult(result);*/
         }
 
         
         [HttpGet("{PollId}/{QId}", Name = "GetSondage")]
         public IActionResult GetSondageById(int PollId, int QId)
         {
-            String answer = null;
-            var dbSondage = _context.SimpleSondageDAOs.FirstOrDefault(t => t.Id == 1);
-            if (dbSondage == null)
-            {
-                return NotFound();
-            }
-
-            PollQuestion question = dbSondage.GetNextQuestion(PollId, QId);
-            //foreach(PollQuestion question in dbSondage.GetNextQuestion(PollId, QId))
-            answer = "Sondage numéro : " + question.PollId 
-                + Environment.NewLine + "Question numéro : " + question.QuestionId 
-                + Environment.NewLine + question.Text 
-                + Environment.NewLine;
-            return new ObjectResult(answer);
+            // Récupération des descriptions
+            PollQuestion question = sondage.GetNextQuestion(PollId, QId-1);
+            // Formatage
+            string contenu = JsonConvert.SerializeObject(question);
+            // Retour de GET
+            if (!contenu.Equals("null"))
+                return Ok(contenu);
+            else return NotFound();
+                                                                                                // Formatage de la réponse
+                                                                                                //foreach(PollQuestion question in dbSondage.GetNextQuestion(PollId, QId))
+                                                                                                /*answer = "Sondage numéro : " + question.PollId 
+                                                                                                    + Environment.NewLine + "Question numéro : " + question.QuestionId 
+                                                                                                    + Environment.NewLine + question.Text 
+                                                                                                    + Environment.NewLine;
+                                                                                                return new ObjectResult(answer);*/
         }
 
-       
-
-        // GET api/sondages/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/sondages
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult PostAnswer([FromBody]PollQuestion answer)
         {
-        }
-
-        // PUT api/sondages/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/sondages/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            if (answer != null)
+            {
+                sondage.SaveAnswer(1, answer);
+                return Ok();
+            }
+            else return BadRequest();
         }
     }
 }
